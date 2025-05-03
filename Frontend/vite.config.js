da6e34d18ecabad49011791907c46be36643306b
Anushka Siyambalapitiya <132772997+Anushkasiyambalapitiya@users.noreply.github.com>
2025-05-03 22:27:38 +0530

    Update con1 vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
- 
+  build: {
+    outDir: 'dist', // Change this to match what Vercel expects
+    emptyOutDir: true
+  },
+  base: './' // Important for correct asset paths
 })
